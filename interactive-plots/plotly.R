fb5cf23ae7542db06fe2d11fc55c018bbebf6467
Ricardo Vladimiro <ricardo.vladimiro@miniclip.com>
2016-05-24 21:35:29 +0100

    Quick test over plotly

---
+++ interactive-plots/plotly.R
@@ -0,0 +1,23 @@
+# Clean it up ------------------------------------------------------------------
+
+rm(list = ls())
+require(plotly)
+
+# Create the dataset -----------------------------------------------------------
+
+# Fake monetisation vars
+fmv <- data.frame(
+    date = as.Date("2016-01-01") + 0:30,
+    ARPDAU = rnorm(n = 31, mean = 0.5, sd = 0.01),
+    ARPPU = rnorm(n = 31, mean = 6, sd = 1.5),
+    cRate = rnorm(n = 31, mean = 0.01, sd = 0.0025)
+)
+
+# Show some lines --------------------------------------------------------------
+
+subplot(
+    plot_ly(fmv, type = "line", x = date, y = ARPDAU, name = "ARPDAU"),
+    plot_ly(fmv, type = "line", x = date, y = ARPPU, name = "ARPPU"),
+    plot_ly(fmv, type = "line", x = date, y = cRate, name = "Conversion Rate"),
+    margin = 0.05
+) %>% layout(showlegend = F)
